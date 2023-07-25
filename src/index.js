c5dd95e26e0246fcb83d4396461a5c3ccebb1e67
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-07-25 21:25:51 +0200

    Create the main-function that will be in charge of invoking all the necessary functions in order for the program to run

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import { createNavBar } from "./navBar.js";
+import "./style.css";
 
+function mainFunction() {
+  const htmlBody = document.querySelector("body");
+  const navDiv = document.createElement("div");
+  navDiv.classList.add("nav-div");
+
+  htmlBody.appendChild(createNavBar());
+}
+
+mainFunction();
