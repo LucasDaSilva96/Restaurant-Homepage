8eeb08fbffd0e1a2ba811bee034471db6db5a0fc
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-07-25 21:24:34 +0200

    Create the function that will render the navbar

---
+++ src/navBar.js
@@ -0,0 +1,21 @@
+export function createNavBar() {
+  const nav = document.createElement("nav");
+  nav.classList.add("navBar");
+
+  nav.innerHTML = `
+  <div class="h1-box">
+  <h1>Restaurant C'est La Vie</h1>
+  </div>
+
+  
+  <div class="list-div">
+  <ul>
+  <li id="home">Home</li>
+  <li id="menu">Menu</li>
+  <li id="contact">Contact</li>
+  </ul>
+  </div>
+  `;
+
+  return nav;
+}
