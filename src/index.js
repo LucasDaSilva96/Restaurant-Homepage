1fec82e323d027320ff8c79b8ebe602ed915ccb1
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-07-26 22:14:51 +0200

    Create the function in charge of removing the second children of the body in order to append a new child to the body. Add a EventListener to the menu-tab in the navbar in order to invoke the createMenuSection function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createNavBar } from "./navBar.js";
 import { createHeroSection } from "./hero.js";
+import { createMenuSection } from "./menu.js";
 
 import "./style.css";
 
@@ -8,8 +9,29 @@ function mainFunction() {
   const navDiv = document.createElement("div");
   navDiv.classList.add("nav-div");
 
+  // Homepage
   htmlBody.appendChild(createNavBar());
   htmlBody.appendChild(createHeroSection());
+  // ********
+
+  // Menu Section
+  const menuTab = document.getElementById("menu");
+  menuTab.addEventListener("click", function () {
+    removeSecondChildEl();
+    htmlBody.appendChild(createMenuSection());
+  });
 }
 
 mainFunction();
+
+function removeSecondChildEl() {
+  // Get a reference to the <body> element
+  const bodyElement = document.body;
+
+  // Check if the body has at least two child elements
+  if (bodyElement.children.length >= 2) {
+    // Get the second child element (index 1) and remove it from the <body>
+    const secondChildElement = bodyElement.children[2];
+    bodyElement.removeChild(secondChildElement);
+  }
+}
