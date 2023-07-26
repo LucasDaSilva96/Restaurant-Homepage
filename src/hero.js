e69e28575929b6abdbde12b44ad1909b38ac2904
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-07-26 21:28:16 +0200

    Add more content to the createHeroSection function

---
+++ src/hero.js
@@ -0,0 +1,20 @@
+import "./style.css";
+export function createHeroSection() {
+  const heroSection = document.createElement("section");
+  heroSection.id = "hero-section";
+
+  heroSection.innerHTML = `
+  <div class="hero-container">
+
+  <div class="hero-image-box"></div>
+
+  <div class="hero-text-box">
+  <p class="hero-text">Welcome to Restaurant C'est La Vie, where the essence of France comes alive on your plate! 
+  Step into a world of culinary delight as we take you on a gastronomic journey inspired by the rich and diverse 
+  flavors of French cuisine.</p>
+  </div>
+  
+  </div>`;
+
+  return heroSection;
+}
